fd33c5f12d2bfbaf769f3fc5df0afedf8aef9a33
Tristan Brown <tgbrown@stanford.edu>
2018-06-29 20:09:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,6 @@
 var playlist = {
   Elton John: 'Tiny Dancer',
   Ed Sheeran: 'Galway Girl',
-  Lady Gaga: 'Born This Way'
 }
 function updatePlaylist (playlist, name, title) {
   playlist[name] = title
